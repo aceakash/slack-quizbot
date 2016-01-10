f2632702294d41b2b0d749c5a5cad705c2cac9f5
Akash <akashkurdekar@gmail.com>
2016-01-10 12:22:38 +0000

    persist scores

--- src/score-handler.js
+++ src/score-handler.js
@@ -3,61 +3,70 @@
 const _ = require('lodash');
 
 const questionHandler = require('./question-handler');
-
+const scoreRepo = require('./score-repo');
 const players = [];
 
 function start(slackChannel) {
-  slackChannel.on('msg', (msgDetails) => {
-    if (msgDetails.prunedText !== 'scores') {
-      return;
-    }
-    const scores = getFormattedScores();
-    slackChannel.send(scores);
-  });
+  scoreRepo.init().then(() => {
+    slackChannel.on('msg', (msgDetails) => {
+      if (msgDetails.prunedText !== 'scores') {
+        return;
+      }
+      const scores = getFormattedScores().then((scores) => {
+        slackChannel.send(scores);
+      });
+    });
 
-  questionHandler.on('correctAnswer', details => {
-    console.log('received event correctAnswer', details);
-    updatePlayer(players, details.userName, details.answerTime);
-    console.log(players);
+    questionHandler.on('correctAnswer', details => {
+      details.ts = new Date().toISOString();
+      scoreRepo.writeScore(details)
+        .catch((err) => {
+            console.error('Could not update score', err);
+        })
+    });
   });
 }
 
 function getFormattedScores() {
-  const players = getPlayersSortedByScore();
-  let result = ":trophy: *Current Scores* :trophy: \n";
-  result += players
-    .map(player => `${getFormattedName(player.userName)} : ${player.score} (avg: ${getAverageAnswerTimeForPlayer(player)} s)`)
-    .join('\n');
-  return result;
+  return new Promise((resolve, reject) => {
+    const scores = scoreRepo.getScores().then((scores) => {
+      scores = Array.prototype.concat([], scores);
+      let result = ":trophy: *Current Scores* :trophy: \n";
+      result += scores
+        .map(score => `${getFormattedName(score.userName)} : ${score.score} (avg: ${score.averageAnswerTimeSec.toFixed(2)} s)`)
+        .join('\n');
+      return resolve(result);
+    });
+  });
 }
 
-function getAverageAnswerTimeForPlayer(player) {
-  const totalTime = player.answerTimes.reduce((prev, curr) => prev + curr);
-  return (totalTime/player.answerTimes.length).toFixed(2);
-}
+// function getAverageAnswerTimeForPlayer(player) {
+//   const totalTime = player.answerTimes.reduce((prev, curr) => prev + curr);
+//   return (totalTime/player.answerTimes.length).toFixed(2);
+// }
 
-function getPlayersSortedByScore() {
-  return _.sortBy(players, 'score').reverse();
-}
+// function getPlayersSortedByScore() {
+//   return _.sortBy(players, 'score').reverse();
+// }
 
 function getFormattedName(name) {
   const letters = name.split('');
   return letters[0] + '-' + letters.splice(1).join('');
 }
 
-function updatePlayer(players, userName, answerTime) {
-  var player = players.filter(p => p.userName === userName)[0];
-  if (!player) {
-    player = {
-      userName: userName,
-      score: 0,
-      answerTimes: []
-    };
-    players.push(player);
-  }
-  player.score += 1;
-  player.answerTimes.push(answerTime);
-}
+// function updatePlayer(players, userName, answerTime) {
+//   var player = players.filter(p => p.userName === userName)[0];
+//   if (!player) {
+//     player = {
+//       userName: userName,
+//       score: 0,
+//       answerTimes: []
+//     };
+//     players.push(player);
+//   }
+//   player.score += 1;
+//   player.answerTimes.push(answerTime);
+// }
 
 module.exports = {
   start
---
+++ src/score-repo.js
@@ -0,0 +1,49 @@
+const sql = require('sqlite3').verbose();
+const db = new sql.Database('scores.db');
+
+module.exports = {
+    init,
+    writeScore,
+    getScores
+};
+
+function init() {
+  return createScoresTableIfNotPresent();
+}
+
+function getScores() {
+  return new Promise((resolve, reject) => {
+      const query = 'select userName, count(1) as score, avg(answerTimeSec) as averageAnswerTimeSec from scores group by username order by score desc';
+      db.get(query, (err, docs) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(docs);
+      });
+  });
+}
+
+function createScoresTableIfNotPresent() {
+  return new Promise(function (resolve, reject) {
+    const createQuery = 'CREATE TABLE IF NOT EXISTS scores (dateTimeUtc TEXT not null, userName TEXT not null, questionId INTEGER not null, answerTimeSec REAL not null)';
+    db.run(createQuery, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+    });
+  });
+}
+
+function writeScore(details) {
+  return new Promise((resolve, reject) => {
+    db.run('insert into scores (dateTimeUtc, userName, questionId, answerTimeSec) values (?, ?, ?, ?)',
+      details.ts, details.userName, details.questionId, details.answerTime, (err) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve();
+        }
+    );
+  });
+}
