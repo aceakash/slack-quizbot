bf231ae196900a87c2659697a080dc407947d37a
Akash K <akashkurdekar@gmail.com>
2019-02-19 09:49:35 +0000

    retro

--- src/quizballs-repo.js
+++ src/quizballs-repo.js
@@ -1,21 +1,20 @@
-const loki = require('lokijs');
+// const loki = require('lokijs');
 const _ = require('lodash');
 
-const db = new loki('quizballs.json');
+// const db = new loki('quizballs.json');
+const questions = require('./retro-questions.json')
+let i = 0;
 
 function getRandomQuestion() {
   return new Promise((fulfill, reject) => {
-    db.loadDatabase({}, () => {
-      const coll = db.getCollection('quizItems');
-      const doc = coll.get(_.sample(coll.idIndex));
+      
       const question = {
-        q: doc.q,
-        a: doc.a,
-        id: doc.$loki
-      };
-      fulfill(question);
+        q: questions[i].q,
+        a: questions[i].a,
+        id: questions[i]['$loki']
+      }
+      fulfill(question)
     });
-  });
 }
 
 module.exports = {getRandomQuestion};
